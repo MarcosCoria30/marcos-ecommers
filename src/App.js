af3345438c115a62601f6096a186e0c5864e35f7
MarcosCoria30 <marcoscoria30@hotmail.com>
2021-10-12 19:16:20 -0300

    cambios dos

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {NavBar} from "./components/NavBar"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-       <h1>esta es mi aplicacion</h1>
+   
+      <NavBar/>
       
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      
   );
 }
 
